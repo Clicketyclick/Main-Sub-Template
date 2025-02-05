3a8f1f8a6cb172be8fc4db3299e7cad87a71caaf
Erik Bachmann <Erik@Clicketyclick.dk>
2025-02-05 13:32:02 +0100

    $gitInfo in header

--- index.php
+++ index.php
@@ -29,19 +29,58 @@
 
 header( 'Content-type: text/html; charset=utf-8' );
 
+include_once('./lib/getGitInfo.php');
+
+/*
+
+echo "<pre>";
+
+var_export( $gitInfo );
+var_export( $gitInfo['version'] );
+
+echo "</pre>";
+
+
+/*
+$gitVersion     = getGitVersion();
+$gitCommitInfo  = getGitCommitInfo();
+
+//echo "<pre>";
+$gitRepoName    = getGitRepoName();
+var_export( $gitRepoName );
+var_export( $gitVersion );
+var_export( $gitCommitInfo );
+*/
+
 $config = [
   'system' => [
-    'name' => 'Screen-template',
-    'version' => '01.00',
-    'revision' => '2025-02-04T13:27:20',
-    'release' => '2025-02-04T13:27:30',
-    'level' => 'alpha',
-    'language' => 'en',
+    'name'      => $gitRepoName ?? basename( __FILE__ ),
+    'version'   => implode( '.', array_slice($gitInfo['version'] ?? ['?','?','?','?'] , 0, 3) ),
+    'revision'  => $gitInfo['commit']['commitdate'] ?? 'unknown rev',
+    'release'   => $gitInfo['commit']['commitdate'] ?? 'unknown rel.',
+    'level'     => array_pop($gitInfo['version']) ?? 'alpha',
+    'language'  => 'en',
+    ],
+    'icons'     => [
+        'alpha' => "&#x1F9EB;",
+        'beta'  => "&#x1F9EA;",
+        'prod'  => "&#x1F3ED;",
+    ],
+    
+    
+
     /*
+    	"alpha": { "key": "Petri Dish (level)", "html": "&#x1F9EB;", "char": "🧫" },
+	"beta": { "key": "Test tube (level)", "html": "&#x1F9EA;", "char": "🧪" },
+	"_prod": { "key": "Libra (level)", "html": "&#x264E;", "char": "♎" },
+	"prod": { "key": "Factory (level)", "html": "&#x1F3ED;", "char": "🏭" },
+
     'theme' => 'contrast',
     'display' => 'Standard',
     'favicon' => './icons/favicon.ico',
     */
-  ]
 ];
+
 include_once('./screens/'.($_REQUEST['screen']??'screen').'.php');
+
+?>
