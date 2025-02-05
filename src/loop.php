7a50e6f15841fa28c5d096d9e2fb19b135734903
Erik Bachmann <Erik@Clicketyclick.dk>
2025-02-05 13:35:35 +0100

    updated

--- src/loop.php
+++ src/loop.php
@@ -17,9 +17,17 @@ header( 'Content-type: text/html; charset=utf-8' );
 while (@ob_end_flush());
 echo "<script src='../js/screen.js'></script>";
 
+$buffer_script = "";
+js_log( $buffer_script, 'debug', date('c').' ready\n');   // Insert closure
+js_flush( $buffer_script ); // Flush js
+
 loop();
 
 echo 'Finished!';
+$buffer_script = "";
+js_log( $buffer_script, 'debug', date('c').' done\n');   // Insert closure
+js_flush( $buffer_script ); // Flush js
+
 @ob_end_flush(); //end output buffer
 flush(); //-> out everything yet again
 
@@ -27,16 +35,18 @@ flush(); //-> out everything yet again
 function loop($max = 50, $delay = 1 )
 {
     $buffer_script = "";
-    
-    js_report( $buffer_script, 'field_1', date('c'). ' Start' );
+    js_report( $buffer_script, 'start_field', date('c'). ' Start' );
+    js_flush( $buffer_script ); // Flush js
     
     for( $i = 1 ; $i <= $max ; $i++ )
     {
+        $buffer_script = "";    // Clear buffer before building new output
         js_report( $buffer_script, 'review', date('c').' <br />');   // Insert closure
         js_progress( $buffer_script, 'progress', $i, $max );
+        js_log( $buffer_script, 'debug', "Step $i\\n");   // Insert count in debug
         
         js_flush( $buffer_script ); // Flush js
-
+        
         // Current time
         echo (new DateTime('now'))->format('H:i:s.v'), ": $i <br />\n";
 
@@ -45,7 +55,7 @@ function loop($max = 50, $delay = 1 )
         usleep(200000);
     }
     $buffer_script = "";    // Clear buffer
-    js_report( $buffer_script, 'field_2', date('c').' Done');   // Insert closure
+    js_report( $buffer_script, 'end_field', date('c').' Done');   // Insert closure
     js_flush( $buffer_script ); // Flush js
 }
 
@@ -58,6 +68,15 @@ function js_flush( &$buffer_script, $buffers = 64 )
     flush();
 }   // js_flush()
 
+//----------------------------------------------------------------------
+
+function js_log( &$buffer_script, $key, $data )
+{
+    $buffer_script  .= sprintf( "data_log( '%s', '%s' );\n"
+    ,   $key
+    ,   $data
+    );
+}   // js_log()
 //----------------------------------------------------------------------
 
 function js_report( &$buffer_script, $key, $data )
